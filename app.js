307faaa96da3601fb2a8d07dde0cd2834b0f7141
Devan Proust <you@example.com>
2021-10-16 10:47:01 -0700

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,60 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
+//passport config
+require('./config/passport')(passport);
+
+//database
+const db = require('./config/keys').MongoURI;
+
+// connect to mongodb
+
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+// ejs
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// parser
+app.use(express.urlencoded({ extended: false}));
+
+
+
+// express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// middleware for passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connect flash
+app.use(flash());
+
+//global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
 //routes
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://Meme:Meme69@cluster.fnivr.mongodb.net/Cluster?retryWrites=true&w=majority'
+}
